27b7ee849bf7ae23181c9f7a07ca0416225580f5
Daniel Zoné <Dannezone@gmail.com>
2021-03-07 22:44:54 +0100

    Copy pastade det vi gjort för att uppdatera git

--- BaristaApi/Beverage.cs
+++ BaristaApi/Beverage.cs
@@ -1,20 +1,205 @@
+using System;
 using System.Collections.Generic;
 
-public interface IBeverage{
-	List<string> Ingredients { get; }
+public interface IBeverage
+{
+    List<string> Ingredients { get; }
     string CupType { get; }
+}
+
+public interface ICoffeemaker
+{
+    ICoffeemaker addWater(int amount);
+
+    ICoffeemaker addMilk(int amount);
+
+    ICoffeemaker addMilkFoam(int amount);
+
+    ICoffeemaker addChocolateSyrup(int amount);
+
+    ICoffeemaker addBeans(int amount);
+
+    IBeverage ToBeverage();
+}
+
+class CoffeeMaker : ICoffeemaker
+{
+    public int Water { get; set; }
+
+    public int Milk { get; set; }
+
+    public int MilkFoam { get; set; }
+
+    public int ChocolateSyrup { get; set; }
+
+    public int Temperature { get; set; }
+
+    public int Beans { get; set; }
+
+    public List<string> Ingredients = new List<string>();
+
+    public ICoffeemaker addBeans(int amount)
+    {
+        Beans = amount;
+        Ingredients.Add("Beans");
+        Console.WriteLine("Lägger till bönor");
+
+        return this;
+    }
+
+    public ICoffeemaker addWater(int amount)
+    {
+        Water = amount;
+        Ingredients.Add("Water");
+        Console.WriteLine("Lägger till vatten");
+
+        return this;
+    }
+
+    public ICoffeemaker addMilk(int amount)
+    {
+        Milk = amount;
+        Ingredients.Add("Milk");
+
+        return this;
+    }
+
+    public ICoffeemaker addMilkFoam(int amount)
+    {
+        MilkFoam = amount;
+        Ingredients.Add("Milk Foam");
+
+        return this;
+    }
+
+    public ICoffeemaker addChocolateSyrup(int amount)
+    {
+        ChocolateSyrup = amount;
+        Ingredients.Add("Chocolate Syrup");
+
+        return this;
+    }
+
+    public ICoffeemaker addBeans()
+    {
+        return this;
+    }
+
+    public IBeverage ToBeverage()
+    {
+        if (Ingredients.Contains("Milk") && Ingredients.Contains("Chocolate Syrup"))
+        {
+
+            Console.WriteLine("Gör Mocha!");
+
+            return new Mocha();
+        }
+
+        else if (Ingredients.Contains("Milk") && Ingredients.Contains("Milk Foam"))
+        {
+
+            Console.WriteLine("Gör Cappuccino!");
+
+            return new Cappuccino();
+        }
+
+        else if (Ingredients.Contains("Milk Foam"))
+        {
+
+            Console.WriteLine("Gör Macchiato!");
+
+            return new Macchiato();
+        }
+
+        else if (Ingredients.Contains("Water"))
+        {
+
+            Console.WriteLine("Gör Americano!");
+
+            return new Americano();
+        }
+        
+        else if (Ingredients.Contains("Milk"))
+        {
+
+            Console.WriteLine("Gör latte!");
+
+            return new Latte();
+        }
+
+        return new CustomCoffee();
+    }
+}
+
+class Validate
+{
+    public int Temperature { set; get; }
+
 }
 
 class Espresso : IBeverage
 {
-    public List<string> Ingredients => throw new System.NotImplementedException();
+
+    public List<string> Ingredients => new List<string>() { "Beans" };
 
     public string CupType => throw new System.NotImplementedException();
+
 }
 
 class Latte : IBeverage
 {
-    public List<string> Ingredients => throw new System.NotImplementedException();
 
-    public string CupType => throw new System.NotImplementedException();
+    public List<string> Ingredients => new List<string>() { "Milk" };
+    public string CupType => throw new NotImplementedException();
+
 }
+
+class Cappuccino : IBeverage
+{
+
+    public List<string> Ingredients => new List<string>() { "Milk", "Milk Foam" };
+
+    public string CupType => throw new NotImplementedException();
+}
+
+class Americano : IBeverage
+{
+
+    public List<string> Ingredients => new List<string>() { "Water" };
+
+    public string CupType => throw new NotImplementedException();
+}
+
+class Macchiato : IBeverage
+{
+
+    public List<string> Ingredients => new List<string>() { "Milk Foam" };
+
+    public string CupType => throw new NotImplementedException();
+}
+
+class Mocha : IBeverage
+{
+
+    public List<string> Ingredients => new List<string>() { "Milk", "Chocolate Syrup" };
+
+    public string CupType => throw new NotImplementedException();
+}
+
+
+class CustomCoffee : IBeverage
+{
+    List<string> IBeverage.Ingredients => new List<string>() { "" };
+
+    string IBeverage.CupType => throw new NotImplementedException();
+}
+
+//IBeverage espresso = new FluentEspresso()
+//                        .AddWater(20)
+//                        .AddBeans(new Bean()
+//                        {
+//                            AmountInG = 5,
+//                            Sort = CoffeSorts.Robusta
+//                        })
+//                        .Validate(e => e.Temerature > 90)
+//                    .ToBeverage();
--- BaristaApi/Program.cs
+++ BaristaApi/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace BaristaApi
 {
@@ -6,6 +8,56 @@ namespace BaristaApi
     {
         static void Main(string[] args)
         {
+            var newCoffee = new CoffeeMaker().addBeans(19).addWater(1).ToBeverage();
+
+            Console.WriteLine(newCoffee.GetType());
+
+            foreach (string ingredient in newCoffee.Ingredients)
+            {
+                Console.WriteLine(ingredient);
+            }
+
+            var mochaCoffee = new CoffeeMaker().addBeans(5).addChocolateSyrup(20).addMilk(199).ToBeverage();
+            Console.WriteLine(mochaCoffee.GetType());
+
+            foreach (string ingredient in mochaCoffee.Ingredients)
+            {
+                Console.WriteLine(ingredient);
+            }
+
+            var customCoffee = new CoffeeMaker().addBeans(6).addChocolateSyrup(10).addMilk(7).addMilk(19).ToBeverage();
+
+            Console.WriteLine(customCoffee.GetType());
+
+            foreach (string ingredient in customCoffee.Ingredients)
+            {
+                Console.WriteLine(ingredient);
+            }
+
+            var list1 = new List<string>() { "test" };
+            var list2 = new List<string>() { "test", "test2" };
+
+
+            var firstNotSecond = list1.Except(list2).ToList();
+            Console.WriteLine();
+            foreach (var val in firstNotSecond)
+            {
+                Console.WriteLine(val);
+            }
+
+            //coffeeMaker.ToBeverage();
+
+            // IBeverage espresso = new Espresso();
+
+            //IBeverage espresso = new FluentEspresso()
+            //                        .AddWater(20)
+            //                        .AddBeans(new Bean()
+            //                        {
+            //                            AmountInG = 5,
+            //                            Sort = CoffeSorts.Robusta
+            //                        })
+            //                        .Validate(e => e.Temerature > 90)
+            //                    .ToBeverage();
             /* pseudo-code
             Espresso espresso = new Espresso().AddWater(20).AddBeans(b => b.AmountInG = 5 && b.Sort = CoffeSorts.Robusta).ToBravage();
             //espresso is type of Espresso
